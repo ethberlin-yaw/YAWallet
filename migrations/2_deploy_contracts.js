f1515712b19af483c7b6edfc3e577063a795bff6
JavierAguerri <javieraguerrimoreno@gmail.com>
2019-08-24 02:14:18 +0200

    added migrations/2_deploy_contracts.js

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,12 @@
+// Fetch the Storage contract data from the Storage.json file
+var Wallet = artifacts.require("./Wallet.sol");
+var ECTools = artifacts.require("./libs/ECTools.sol");
+
+// JavaScript export
+module.exports = function(deployer) {
+    // Deploy the contract to the network
+    deployer.deploy(ECTools);
+	deployer.link(ECTools, Wallet);
+    deployer.deploy(Wallet);
+
+}
